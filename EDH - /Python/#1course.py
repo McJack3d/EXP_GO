a9265b4032b5323131871432c7d10c0732069cb2
Alexandre Bredillot <alexandrebredillot@gmail.com>
2025-09-08 23:36:24 +0200

    08/09/2025

---
+++ EDH - /Python/#1course.py
@@ -0,0 +1,33 @@
+#prgm 1
+print ("Hello Python !\
+\nThis is my first programming code\
+\nI am looking forward to write more ...")
+
+#prgm 2
+s_min = 60
+min_h = 60
+h_d = 24
+d_w = 7
+w = 5
+number_sec_in5week = s_min * min_h * h_d * d_w * w
+print ("The number of seconds in 5 weeks is " +str(number_sec_in5week))
+
+#prgm 3
+print ("What are you current savings")
+savings = input ()
+expected_savingsy1 = int(savings) * 1.0255
+print ("Your expected savings at the end of the year : "+str(expected_savingsy1))
+
+#prgm 4
+print ("Whats the first number ?")
+first_number = int(input ())
+print ("Whats the second number ?")
+second_number = int(input ())
+sum_step1 = first_number + second_number
+print ("The sum so far is " + str(sum_step1))
+print ("Whats the third number ?")
+third_number = int(input ())
+sum_step2 = sum_step1 + third_number
+print (f"The total sum is "+str(sum_step2))
+
+#prgm 5
