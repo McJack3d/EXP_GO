d5bcf755fb4aed66dade277e7895ebf03ec4977b
Alexandre Bredillot <alexandrebredillot@gmail.com>
2025-09-09 23:34:12 +0200

    09/09/2025
    Implemented solutions for EDHEC email address formatting, EOQ calculation, and boolean value checks in #1course.py. Added 'Exercise Set 1:' file containing exercise descriptions and requirements.

--- EDH - /Python/#1course.py
+++ EDH - /Python/#1course.py
@@ -1,3 +1,4 @@
+"""
 #prgm 1
 print ("Hello Python !\
 \nThis is my first programming code\
@@ -31,3 +32,31 @@ sum_step2 = sum_step1 + third_number
 print (f"The total sum is "+str(sum_step2))
 
 #prgm 5
+print ("Enter your First name : ")
+firstname = input ()
+print ("Enter your Last name :")
+lastname = input ()
+print (f"Your edhec email adress :\n"+(str(firstname)+str(lastname)).lower().replace(" ","")+"@edhec.com")
+
+#prgm 6
+print ("Whats the yearly demand for the product :")
+ydp = int(input ())
+print ("Whats the fixed ordering cost paid per order :")
+foc = int(input ())
+print ("Whats the annual holding cost per unit of product :")
+ahc = int(input ())
+ooq = ((2 * ydp * foc)/ahc) ** 0.5
+print (f"The optimal order quantity is {ooq}")
+"""
+
+#prgm 7 (a)
+first_value = True 
+second_value = True
+both_true = first_value and second_value
+
+print(f"Both values are true : {both_true}")
+#prgm 7(b)
+third_value = False
+three_true = both_true and third_value
+print(f"The three booleans carry the same value : {three_true}")
+ 
