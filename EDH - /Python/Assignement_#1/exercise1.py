852835fea1b97d2cecd1a5e2176a5dc39bbe0a0d
Alexandre Bredillot <alexandrebredillot@gmail.com>
2025-09-24 13:24:21 +0200

    24/09/2025

---
+++ EDH - /Python/Assignement_#1/exercise1.py
@@ -0,0 +1,45 @@
+#a)
+years_similar = 0
+years_lower = 0
+years_total = 0
+grade = ""
+
+def salary_grade(years_similar, years_lower):
+    years_total = years_lower ** 0.5 + years_similar
+    if years_total < 2 :
+        grade = "A"
+    if 2 <= years_total <= 5 :
+        grade = "B"
+    if 6 <= years_total <= 11 :
+        grade = "C"
+    if 12 <= years_total <= 19 :
+        grade = "D"
+    elif years_total >= 20 :
+        grade = "E"
+    return grade
+
+#b)
+role = ""
+referenceA= { "A" : 2700, "B" : 2900, "C" : 3300, "D" : 3600, "E" : 3900}
+referenceS= { "A" : 3800, "B" : 4200, "C" : 4400, "D" : 4700, "E" : 5100}
+mini_salary = 0
+
+def minimum_salary(role, grade):
+
+    if role == "analyst" :
+        referenceA[grade] = mini_salary
+    else :
+        referenceS[grade] =mini_salary
+
+    return mini_salary
+        
+#c)
+status = str(input("Calculation for a new employee(y/n) :")).lower()
+
+if status == "y" :
+    years_similar = int(input("Similar role experience in years : "))   
+    years_lower = int(input("Lower role experience in years : "))
+    role = str(input("Intended role of the employee(analyst/senioranalyst): ")).lower()
+    grade = salary_grade(years_similar, years_lower)
+
+    print(f"The minimum salary of this employee should be:" + str(minimum_salary(role, grade)))
