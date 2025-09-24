575d21a8a8f9b02ce204a84558eb9cf77ff8b5dd
Alexandre Bredillot <alexandrebredillot@gmail.com>
2025-09-24 14:01:13 +0200

    24/09/2025 1

--- EDH - /Python/Assignement_#1/exercise1.py
+++ EDH - /Python/Assignement_#1/exercise1.py
@@ -5,14 +5,14 @@ years_total = 0
 grade = ""
 
 def salary_grade(years_similar, years_lower):
-    years_total = years_lower ** 0.5 + years_similar
+    years_total = years_lower * 0.5 + years_similar
     if years_total < 2 :
         grade = "A"
-    if 2 <= years_total <= 5 :
+    elif 2 <= years_total <= 5 :
         grade = "B"
-    if 6 <= years_total <= 11 :
+    elif 6 <= years_total <= 11 :
         grade = "C"
-    if 12 <= years_total <= 19 :
+    elif 12 <= years_total <= 19 :
         grade = "D"
     elif years_total >= 20 :
         grade = "E"
@@ -22,24 +22,32 @@ def salary_grade(years_similar, years_lower):
 role = ""
 referenceA= { "A" : 2700, "B" : 2900, "C" : 3300, "D" : 3600, "E" : 3900}
 referenceS= { "A" : 3800, "B" : 4200, "C" : 4400, "D" : 4700, "E" : 5100}
-mini_salary = 0
 
 def minimum_salary(role, grade):
 
     if role == "analyst" :
-        referenceA[grade] = mini_salary
+        return referenceA[grade]
     else :
-        referenceS[grade] =mini_salary
-
-    return mini_salary
+        return referenceS[grade]
         
 #c)
 status = str(input("Calculation for a new employee(y/n) :")).lower()
 
 if status == "y" :
-    years_similar = int(input("Similar role experience in years : "))   
+    years_similar = int(input("Similar role experience in years : "))
     years_lower = int(input("Lower role experience in years : "))
     role = str(input("Intended role of the employee(analyst/senioranalyst): ")).lower()
     grade = salary_grade(years_similar, years_lower)
 
     print(f"The minimum salary of this employee should be:" + str(minimum_salary(role, grade)))
+
+elif status != "y" :
+    role = str(input("What is the employee role(analyst/senioranalyst): ")).lower()
+    grade = str(input("What is the employee seniority grade(A/B/C/D/E): ")).capitalize()
+    current_salary = int(input("What is the employee current salary: "))
+
+    if minimum_salary(role, grade) > current_salary :
+        high_salary = (minimum_salary(role, grade) - current_salary)
+        print("{(minimum_salary(role, grade) - current_salary)}should)
+    elif current_salary > minimum_salary(role, grade):
+        print(str(minimum_salary(role, grade) - current_salary))
