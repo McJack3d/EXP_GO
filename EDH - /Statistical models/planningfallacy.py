7bfd9662d143cdb7e0e1ac53ced82ebc1e56c7d3
Alexandre Bredillot <alexandrebredillot@gmail.com>
2025-09-27 13:18:11 +0200

    Add standard error and p-value calculation for planning
    Added computation of the standard error and p-value for the 'Planning' variable to assess whether its expectation differs from zero. The results indicate no evidence against the null hypothesis, suggesting no planning fallacy in this context.

--- EDH - /Statistical models/planningfallacy.py
+++ EDH - /Statistical models/planningfallacy.py
@@ -15,3 +15,17 @@ stats.probplot(df["Planning"],plot=plt)
 
 print(variance)
 
+## Average is an estimate of the expectation
+#The expectation of the "Planning" variable is 1.0188679245283019 (same value as the mean)
+
+#The standard error of the estimator is squared root of (the empirical variance divided by the population N(sample size))
+
+N = len(df) #Sample size
+
+standard_error = np.sqrt(variance / N)
+print(standard_error)
+
+p_value = 1-stats.norm.cdf(mean, loc=0, scale=standard_error)
+print(p_value)
+
+#The p-value is way larger than 0.05. We do not reject the Null hypothesis that the expectation is equal to 0. We conclude that the sample does not provide evidence in favor of a planning fallacy in this context.
