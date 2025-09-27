3667637083b4c458ff0c1c187eeaceff8eb317d6
Alexandre Bredillot <alexandrebredillot@gmail.com>
2025-09-27 14:56:10 +0200

    Add programs for rectangle area, savings, and factorial
    Added three new programs: one to calculate the area of a rectangle, one to compute expected savings after a year with interest, and one to compute the factorial of a given number. Also made minor formatting adjustments to existing code.

--- EDH - /Python/#3course.py
+++ EDH - /Python/#3course.py
@@ -16,7 +16,7 @@ while power * k < u :
     power *= k
 
 print(f"The largest power of {k} strictly below {u} is {power}")
-"""
+
 #prgm 2
 print("Please enter each number of the serie you want to see averaged(enter q when you wanna quit) : ")
 x = (input("Which number to add : "))
@@ -27,3 +27,34 @@ while x != "q" :
     if x = 
 else :
     print(f"The average of your serie is {x}")
+
+#prgm 3
+length = float(input("Input the length of the rectangle :"))
+width = float(input("Input the width of the rectangle :"))
+
+def rectangle_area(length, width):
+    area = length * width
+    return area
+
+print(rectangle_area(length, width))
+
+#prgm 4
+savings = float(input("Input your actual saving : "))
+expected_rr = float(input("Input the expected interest rate for the next year (in %): "))
+
+def compute_savings(savings, expected_rr):
+    savings_y1 = savings * (expected_rr/100) +savings
+    return savings_y1
+
+print(f"The expected total savings after year 1 should be : {compute_savings(savings, expected_rr)} ")
+"""
+#prgm 5
+#a)
+base = int(input("Input the number you wanna see the factorial of :"))
+
+def compute_factorial(base) : 
+    for base in base :
+     n =+ base 
+    return n  
+
+print(compute_factorial(base))
