567d222368111ab9c81f04387d0805e295601061
Alexandre Bredillot <alexandrebredillot@gmail.com>
2025-09-27 12:42:07 +0200

    Fix variance calculation and update plotting in planningfallacy.py
    Set ddof=1 in variance calculation for sample variance. Commented out plt.show() and added a note about normality based on QQ plot. Added print statement for variance output.

--- EDH - /Statistical models/planningfallacy.py
+++ EDH - /Statistical models/planningfallacy.py
@@ -6,8 +6,12 @@ from scipy import stats
 df = pd.read_excel("/Users/alexandrebredillot/Documents/GitHub/EXP_GO/EDH - /Statistical models/Planning_fallacy_Wooclap.xlsx")
 
 mean = df["Planning"].mean()
-variance= df["Planning"].var()
+variance= df["Planning"].var(ddof=1)
 
 stats.probplot(df["Planning"],plot=plt)
-plt.show()
+#plt.show()
+
+#After a brief review of the qqplot we can conclude that the variables follow a normal distribution
+
+print(variance)
 
