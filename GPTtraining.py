e405c778bcc2e17d3b893743987cf4f1c9cc0fee
Alexandre Bredillot <alexandrebredillot@gmail.com>
2025-09-27 16:09:19 +0200

    Add script for integer parity check
    Introduces GPTtraining.py, which prompts the user for an integer and prints True if the number is odd, False if even. Also includes a commented-out section for greeting the user by name.

---
+++ GPTtraining.py
@@ -0,0 +1,13 @@
+"""
+name = str(input("Please enter your name: "))
+
+print(f"Hello {name} and welcome to the world of coding !")
+"""
+n = int(input("Please input an integer number: "))
+
+if n%2 == 0 :
+    result = False
+else : 
+    result = True
+
+print (result)
