ad1687c99c7347b0fe38f168388a44935012a421
Alexandre Bredillot <alexandrebredillot@gmail.com>
2025-10-27 15:51:09 +0100

    Switch to extended Boston housing dataset in lecture1.py
    Replaced the synthetic wave dataset with the extended Boston housing dataset loaded from an Excel file. Updated the plotting range for the regression line to use a fixed interval.

--- EDH - /Machine Learning/lecture1.py
+++ EDH - /Machine Learning/lecture1.py
@@ -5,8 +5,9 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
-X, y = mglearn.datasets.make_wave(n_samples = 60)
+df = pd.read_excel("/Users/alexandrebredillot/Documents/GitHub/EXP_GO/EDH - /Machine Learning/boston_housing_extended_dataset.xlsx")
 
+X, y = df.iloc[:, :-1].values, df.iloc[:, -1].values
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = 42)
 
 lr = LinearRegression().fit(X_train, y_train)
@@ -16,7 +17,7 @@ plt.scatter(X_train, y_train, label="data")
 plt.scatter(X_test, y_test, label = "test", marker = '^') 
 
 # plot linear regression line
-X_line = np.linspace(X.min(), X.max(), 200).reshape(-1, 1)
+X_line = np.linspace(-3, 3, 1000).reshape(-1, 1)
 y_line = lr.predict(X_line)
 plt.plot(X_line, y_line, color="red", linewidth=2, label="linear fit")
 
