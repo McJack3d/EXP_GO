5a2961171c8520a458a28248b29cd74c533e9acc
Alexandre Bredillot <alexandrebredillot@gmail.com>
2025-11-09 15:43:53 +0100

    Add fraud prediction data and script for HW1
    Added 'fraud_homework_features.csv' and 'fraudpredictor.py' for Data Mining HW1. Also included a preprocessed Boston housing dataset for Machine Learning and updated lecture1.py. DS_Store files were added/modified, but these are system files and not relevant to code or data.

--- EDH - /Machine Learning/lecture1.py
+++ EDH - /Machine Learning/lecture1.py
@@ -5,24 +5,14 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
-df = pd.read_excel("/Users/alexandrebredillot/Documents/GitHub/EXP_GO/EDH - /Machine Learning/boston_housing_extended_dataset.xlsx")
+df = pd.read_excel("/Users/alexandrebredillot/Documents/GitHub/EXP_GO/EDH - /Machine Learning/boston_housing_preprocessed_dataset.xlsx")
+
+print(df.head())
 
 X, y = df.iloc[:, :-1].values, df.iloc[:, -1].values
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = 42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = 0)
 
 lr = LinearRegression().fit(X_train, y_train)
 
-# scatter points
-plt.scatter(X_train, y_train, label="data")
-plt.scatter(X_test, y_test, label = "test", marker = '^') 
+#higher intercept is less good ? 
 
-# plot linear regression line
-X_line = np.linspace(-3, 3, 1000).reshape(-1, 1)
-y_line = lr.predict(X_line)
-plt.plot(X_line, y_line, color="red", linewidth=2, label="linear fit")
-
-plt.xlabel("Feature")
-plt.ylabel("Target")
-plt.legend()
-plt.tight_layout()
-plt.show()
